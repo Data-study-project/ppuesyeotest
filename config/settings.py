c8429c1ff829cbac8e68bc945e226af9d3ada532
hhongjj <hhongjj3@gmail.com>
2021-11-14 19:17:16 +0900

    heroku edited

--- config/settings.py
+++ config/settings.py
@@ -167,10 +167,6 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
-MEDIA_ROOT = BASE_DIR / 'media'
-
-MEDIA_URL = '/media/'
-
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
