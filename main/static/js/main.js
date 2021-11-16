53011f93763a062741f8c3a2244dc50ee71fcdf1
hhongjj <hhongjj3@gmail.com>
2021-11-16 22:03:35 +0900

    kakao

--- main/static/js/main.js
+++ main/static/js/main.js
@@ -9,44 +9,44 @@ $(function(){
 
 
 // kakao.init('{{api_key}}');
-function sendLink() {
-  console.log(Kakao.isInitialized());
-  let result_url = window.location.href;
-  Kakao.Link.sendDefault({
-    objectType: 'feed',
-    content: {
-      title: '나의 건강 상태는?',
-      description: '나의 현재 건강 상태는 어떨까?',
-      imageUrl: 'https://ppusyeo.herokuapp.com/', 
-      link: {
-        mobileWebUrl: 'https://ppusyeo.herokuapp.com/',
-        webUrl: 'https://ppusyeo.herokuapp.com/',
-      },
-    },
+// function sendLink() {
+//   console.log(Kakao.isInitialized());
+//   let result_url = window.location.href;
+//   Kakao.Link.sendDefault({
+//     objectType: 'feed',
+//     content: {
+//       title: '나의 건강 상태는?',
+//       description: '나의 현재 건강 상태는 어떨까?',
+//       imageUrl: 'https://ppusyeo.herokuapp.com/', 
+//       link: {
+//         mobileWebUrl: 'https://ppusyeo.herokuapp.com/',
+//         webUrl: 'https://ppusyeo.herokuapp.com/',
+//       },
+//     },
     
-    social: {
-      likeCount: 100,
-      commentCount: 10,
-      sharedCount: 100,
-    },
-    buttons: [
-      {
-        title: '결과 확인하기',
-        link: {
-          webUrl: result_url,
-          mobileWebUrl: result_url,
-        },
-      },
-      {
-        title: '테스트 해보기',
-        link: {
-          webUrl: 'https://ppusyeo.herokuapp.com/',
-          mobileWebUrl: 'https://ppusyeo.herokuapp.com/',
-        },
-      },
-    ]
-  });
-};
+//     social: {
+//       likeCount: 100,
+//       commentCount: 10,
+//       sharedCount: 100,
+//     },
+//     buttons: [
+//       {
+//         title: '결과 확인하기',
+//         link: {
+//           webUrl: result_url,
+//           mobileWebUrl: result_url,
+//         },
+//       },
+//       {
+//         title: '테스트 해보기',
+//         link: {
+//           webUrl: 'https://ppusyeo.herokuapp.com/',
+//           mobileWebUrl: 'https://ppusyeo.herokuapp.com/',
+//         },
+//       },
+//     ]
+//   });
+// };
 
 function copyUrl(){
   let tmp = document.createElement('input');
@@ -68,4 +68,4 @@ function sharefacebook(){
   window.open(link);
 }
 
-kakao_share.addEventListener('click', sendLink)
+// kakao_share.addEventListener('click', sendLink)
